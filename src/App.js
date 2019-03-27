de19a5ad99f4c47a0afa9ca08d29c4202f8799a3
AJ Almaguer <a.almaguer@farmersinsurance.com>
2019-03-26 23:28:34 -0700

    connected Node component to store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Provider } from 'react-redux'
 
+import './App.css';
 import { configureStore } from './configureStore';
+import { selectNode, createNode, addChild } from './actions';
 import Node from './containers/Node';
-import { selectNode, createNode, addChild, removeChild, deleteNode } from './actions';
 
 
 const store = configureStore();
@@ -12,26 +13,14 @@ store.dispatch(selectNode('123'))
 store.dispatch(selectNode('456'))
 
 let { nodeId: parentId } = store.dispatch(createNode());
-console.log('parentId =', parentId);
-
 let { nodeId: childId1 } = store.dispatch(createNode());
-console.log('childId1 =', childId1);
-
 let { nodeId: childId2 } = store.dispatch(createNode());
-console.log('childId2 =', childId2);
-
 let { nodeId: childId3 } = store.dispatch(createNode());
-console.log('childId3 =', childId3);
 
 store.dispatch(addChild(parentId, childId1));
 store.dispatch(addChild(parentId, childId2));
 store.dispatch(addChild(childId2, childId3));
 
-store.dispatch(removeChild(parentId, childId2));
-store.dispatch(deleteNode(childId2));
-
-
-console.log('store =', store.getState());
 
 
 class App extends Component {
@@ -40,12 +29,19 @@ class App extends Component {
             <div>
                 <h1 style={{ textAlign: "center" }}>Evolutionary Tree</h1>
 
-                <div className="tree">
+                <Provider store={store}>
+                    <div className="tree">
+                        <ul>
+                            <Node id={parentId} />
+                        </ul>
+                    </div>
+                </Provider>
+
+                {/* <div className="tree">
                     <ul>
                         <li>
                             <div className="node">Parent</div>
                             <ul>
-                                <Node />
                                 <li>
                                     <div className="node">Child</div>
                                     <ul>
@@ -78,7 +74,7 @@ class App extends Component {
                             </ul>
                         </li>
                     </ul>
-                </div>
+                </div> */}
             </div>
 
 
--- src/containers/Node.js
+++ src/containers/Node.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 class Node extends Component {
+    handleNameChange = (e) => {
+        console.log('value =', e.target.value);
+    }
+
+    handleAddChildClick = () => {
+        console.log('add child');
+    }
+
+    handleRemoveClick = () => {
+        console.log('remove child');
+    }
+
     render() {
         return (
             <li>
                 <div className="node">
                     <div>
-                        <input type="text" placeholder="Name..." />
+                        <input type="text" placeholder="Name..." onChange={this.handleNameChange} />
                     </div>
-                    
-                    <button type="button" className="primary">Add Child</button>
-                    <button type="button" className="danger">Remove</button>
+
+                    <button 
+                        type="button" 
+                        className="primary"
+                        onClick={this.handleAddChildClick}
+                    >
+                        Add Child
+                    </button>
+                    <button 
+                        type="button" 
+                        className="danger"
+                        onClick={this.handleRemoveClick}
+                    >
+                        Remove
+                    </button>
                 </div>
                 {/* <ul> */}
                 {/* children go here */}
@@ -23,9 +47,10 @@ class Node extends Component {
 }
 
 
-// function mapStateToProps(state, ownProps) {
-//     return state[ownProps.id]
-// }
+function mapStateToProps(state, ownProps) {
+    console.log('>>> state =', state.nodesById);
+    return state.nodesById[ownProps.id]
+}
 
-// const ConnectedNode = connect(mapStateToProps, actions)(Node);
-export default Node;
+const ConnectedNode = connect(mapStateToProps)(Node);
+export default ConnectedNode;
