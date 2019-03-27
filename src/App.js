2f5c5c56a95c36beaa53a754b91c7e3909baf101
AJ Almaguer <a.almaguer@farmersinsurance.com>
2019-03-26 21:06:37 -0700

    added tree html and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="tree">
+        <ul>
+          <li>
+            <a>Parent</a>
+            <ul>
+              <li>
+                <a>Child</a>
+                <ul>
+                  <li>
+                    <a>Grand Child</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Child</a>
+                <ul>
+                  <li><a>Grand Child</a></li>
+                  <li>
+                    <a>Grand Child</a>
+                    <ul>
+                      <li>
+                        <a>Great Grand Child</a>
+                      </li>
+                      <li>
+                        <a>Great Grand Child</a>
+                      </li>
+                      <li>
+                        <a>Great Grand Child</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a>Grand Child</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     );
   }
