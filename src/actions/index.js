93f554020c9297aba129c5bc99dcf0a2ddcff6db
AJ Almaguer <a.almaguer@farmersinsurance.com>
2019-03-27 00:00:55 -0700

    format

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const ADD_CHILD = 'ADD_CHILD';
 export const REMOVE_CHILD = 'REMOVE_CHILD';
 
 
-export  const selectNode = (id) => ({
+export const selectNode = (id) => ({
     type: SELECT_NODE,
     id
 });
