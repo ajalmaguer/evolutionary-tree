0e081de2ed4ec9c6b9a729891d9b6616c5cae596
AJ Almaguer <a.almaguer@farmersinsurance.com>
2019-03-27 04:41:16 -0700

    styled title

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1 style={{ textAlign: "center" }}>Evolutionary Tree</h1>
+                <h1 id="title">Evolutionary Tree</h1>
 
                 <Provider store={store}>
                     <Router>
