b4854181d1f29db556f1bb69ec087e5f3d24cbd4
AJ Almaguer <a.almaguer@farmersinsurance.com>
2019-03-27 23:15:37 -0700

    updated env config for heroku db

--- server-src/config/env.js
+++ server-src/config/env.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 
 module.exports.PORT = process.env.PORT || 4200;
-module.exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/evolutionary-tree';
+module.exports.DATABASE_URL = process.env.MONGODB_URI || process.env.DATABASE_URL || 'mongodb://localhost/evolutionary-tree';
