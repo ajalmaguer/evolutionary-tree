96923a4949b00c7748c4e06e2c8fa79ad6280bd4
AJ Almaguer <a.almaguer@farmersinsurance.com>
2019-03-28 00:26:09 -0700

    converted Remove Button to use hooks

--- src/components/RemoveButton.js
+++ src/components/RemoveButton.js
@@ -1,56 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 
-class RemoveButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            removeFlag: false
-        };
-    }
+const RemoveButton = ({ remove, id }) => {
+    const [removeFlag, setRemoveFlag] = useState(false);
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.id !== this.props.id) {
-            this.setFlag(false);
-        }
-    }
+    useEffect(() => {
+        setRemoveFlag(false);
+    }, [id]);
 
-    setFlag = (value) => {
-        this.setState({removeFlag: value});
-    }
-
-    render() {
-        const { removeFlag } = this.state;
-
-        if (!removeFlag) {
-            return (
+    if (!removeFlag) {
+        return (
+            <button
+                type="button"
+                className="danger"
+                onClick={() => setRemoveFlag(true)}
+            >
+                Remove Me !!!!!
+                </button>
+        );
+    } else {
+        return (
+            <div>
                 <button
                     type="button"
-                    className="danger"
-                    onClick={() => this.setFlag(true)}
+                    onClick={() => setRemoveFlag(false)}
                 >
-                    Remove Me !!!!!
-                </button>
-            );
-        } else {
-            return (
-                <div>
-                    <button
-                        type="button"
-                        onClick={() => this.setFlag(false)}
-                    >
-                        Nevermind, Don't Remove
+                    Nevermind, Don't Remove
                     </button>
 
-                    <button
-                        type="button"
-                        onClick={() => this.props.remove()}
-                    >
-                        Yes, Remove Me
+                <button
+                    type="button"
+                    onClick={() => remove()}
+                >
+                    Yes, Remove Me
                     </button>
-                </div>
-            );
-        }
+            </div>
+        );
     }
-};
+}
 
 export default RemoveButton;
